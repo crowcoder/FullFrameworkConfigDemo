a8114331ece567d496e8457c3cb9743eb29e7919
Bob <crowcoder@gmail.com>
2019-03-17 12:46:16 -0400

    Initial commit

---
+++ FullFrameworkConfiguration/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace FullFrameworkConfiguration.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            string machSetting = ConfigurationManager.AppSettings["MachineConfigSetting"];
+            string xternal = ConfigurationManager.AppSettings["ExternalConfigSetting"];
+            string envar = Environment.GetEnvironmentVariable("OS");
+
+            return new string[] { machSetting, envar, xternal };
+        }
+    }
+}
